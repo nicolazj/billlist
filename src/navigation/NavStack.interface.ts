1694956b9e45063796903761db3119023830afdb
Nick Jiang <nick.jiang@sky.co.nz>
2022-10-17 21:11:05 +1300

    update RN

--- src/navigation/NavStack.interface.ts
+++
@@ -1,9 +0,0 @@
-import { StackScreenProps } from '@react-navigation/stack';
-
-/* eslint-disable-next-line @typescript-eslint/consistent-type-definitions */
-export type RootStackParamList = {
-  Home: undefined;
-  BillList: undefined;
-};
-
-export type Props = StackScreenProps<RootStackParamList, 'BillList'>;
